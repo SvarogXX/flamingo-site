a79b4deddf341378069e94b575aa0f788626bf1e
SvarogXX <yaremchuk.0xx@gmail.com>
2026-02-28 11:38:17 +0200

    Update Google Analytics 4 to new GA4 resource tag: G-F5KV6EP19T

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,6 +122,7 @@ export default function RootLayout({
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
               strategy="afterInteractive"
+              async
             />
             <Script
               id="google-analytics"
@@ -131,9 +132,7 @@ export default function RootLayout({
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-                    page_path: window.location.pathname,
-                  });
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
                 `,
               }}
             />
